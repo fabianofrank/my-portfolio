0955a48976ea5c69fd5cdbd250558944fbf351a3
fabianofrank <fabianofrank@protonmail.com>
2022-10-19 12:48:37 -0300

    Add projects

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,17 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => (
-  <nav className="sticky top-0 z-10 w-full bg-gray-500 backdrop-filter backdrop-blur-lg bg-opacity-30">
+  <nav className="sticky top-0 z-10 w-full bg-gray-500 backdrop-filter backdrop-blur-lg bg-opacity-10">
     <div className="max-w-5xl mx-auto px-4">
       <div className="flex items-center justify-between h-16">
-        <span className="text-2xl text-[#e6e6e9] font-semibold">
-          <Link to="/">Home</Link>
+        <span className="text-xl text-[#e6e6e9] font-semibold font-mono">
+          <Link to="/">frankk.dev</Link>
         </span>
         <div className="hidden md:flex space-x-4 text-xl text-[#e6e6e9]">
           <Link to="/about">About</Link>
           <Link to="/projects">Projects</Link>
         </div>
-        <span className="flex md:hidden text-white">Menu</span>
+        <span className="flex text-xl md:hidden text-white">Menu</span>
       </div>
     </div>
   </nav>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,37 +1,144 @@
 import React from 'react';
+import showroom from '../assets/showroom.png';
+import apple from '../assets/apple_detail.png';
+import wallet from '../assets/wallet.png';
+import blog from '../assets/blog_mock.png';
+import pokemon from '../assets/pokemon.png';
+import bookstore from '../assets/bookstore.png';
+import spacetravelers from '../assets/spacetravelers.png';
 
 const Projects = () => (
   <div className="w-full min-w-[360px]">
-    <div className="max-w-[1000px] mx-auto px-8 flex flex-col h-full justify-center items-center">
-      <h1 className="text-[#e6e6e9] text-4xl self-start sm:text-5xl py-6">About Me</h1>
-      <p className="text-[#e6e6e9] text-l self-start sm:text-xl py-3 font-serif">
-        I’m a senior software engineer with over 15 years of experience and currently working as
-        Enginnering Manager at
-        <a href="https://mondu.ai">MONDU</a>
-        , a “Buy Now, Pay Later” for Online B2B Checkout.
-      </p>
-      <p className="text-[#e6e6e9] text-l self-start sm:text-xl py-3 font-serif">
-        Recently worked as Engineering Manager at
-        <a href="https://www.vehiculum.de/">VEHICULUM</a>
-        , one of Germany’s largest car leasing platform.
-      </p>
-      <p className="text-[#e6e6e9] text-l self-start sm:text-xl py-3 font-serif">
-        Prior to that, I worked in several mid to large size companies such as Macy’s,
-        Bloomingdales and GAP. During the time working for GAP, I lived in San Francisco, CA for
-        4 months. Also co-founded a startup called Appontar.
-      </p>
-      <p className="text-[#e6e6e9] text-l self-start sm:text-xl py-3 font-serif">Since 2017 I’ve been living in Berlin, Germany.</p>
-      <p className="text-[#e6e6e9] text-l self-start sm:text-xl py-3 font-serif">
-        Current stack and technologies: Ruby on Rails, PostgreSQL, Redis, Javascript/Vue.js and
-        React, Docker, Kubernetes, Elasticsearch, Terraform and AWS.
-      </p>
-      <p className="text-[#e6e6e9] text-l self-start sm:text-xl py-3 font-serif">
-        <hr className="py-3 text-gray-900 w-screen" />
-        Do you want to say hello? Feel free to
-        <a href="https://twitter.com/dviana19">follow me on Twitter</a>
-        .
-      </p>
-      <p className="text-[#e6e6e9] text-l self-start sm:text-xl py-3 font-serif">Thanks for reading!</p>
+    <div className="max-w-[1000px] mx-auto px-8 flex h-full justify-center items-center">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+
+        <div className="w-96 m-6 shadow-xl bg-gray-200 backdrop-filter backdrop-blur-lg bg-opacity-10 rounded-3xl">
+          <a href="https://github.com/fabianofrank/react-show-room-ui">
+            <figure className="my-6"><img src={showroom} alt="Car Showroom Website" /></figure>
+            <div className="mx-3 flex flex-col">
+              <h2 className="text-[#e6e6e9] font-semibold">Showroom App</h2>
+              <p className="text-[#e6e6e9] font-light">
+                This project is a result from combined development
+                of an API in Ruby on Rails and the UI in React.
+              </p>
+              <div className="self-center">
+                <div className="px-6 pt-4 pb-2">
+                  <span className="inline-block bg-[#e6e6e9] rounded-full px-3 py-1 text-sm font-semibold text-[#1a1b1b] mr-2 mb-2">#rails</span>
+                  <span className="inline-block bg-[#e6e6e9] rounded-full px-3 py-1 text-sm font-semibold text-[#1a1b1b] mr-2 mb-2">#react</span>
+                  <span className="inline-block bg-[#e6e6e9] rounded-full px-3 py-1 text-sm font-semibold text-[#1a1b1b] mr-2 mb-2">#postgresql</span>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+
+        <div className="w-96 m-6 shadow-xl bg-gray-200 backdrop-filter backdrop-blur-lg bg-opacity-10 rounded-3xl">
+          <a href="https://github.com/fabianofrank/rails-blog">
+            <figure className="my-6"><img src={blog} alt="Blog Website" /></figure>
+            <div className="mx-3 flex flex-col">
+              <h2 className="text-[#e6e6e9] font-semibold">Blog App</h2>
+              <p className="text-[#e6e6e9] font-light">
+                A Blog app that shows the list of posts and empower readers to interact with them by
+                adding comments and liking posts
+              </p>
+              <div className="self-center">
+                <div className="px-6 pt-4 pb-2">
+                  <span className="inline-block bg-[#e6e6e9] rounded-full px-3 py-1 text-sm font-semibold text-[#1a1b1b] mr-2 mb-2">#ruby</span>
+                  <span className="inline-block bg-[#e6e6e9] rounded-full px-3 py-1 text-sm font-semibold text-[#1a1b1b] mr-2 mb-2">#rails</span>
+                  <span className="inline-block bg-[#e6e6e9] rounded-full px-3 py-1 text-sm font-semibold text-[#1a1b1b] mr-2 mb-2">#postgresql</span>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+
+        <div className="w-96 m-6 shadow-xl bg-gray-200 backdrop-filter backdrop-blur-lg bg-opacity-10 rounded-3xl">
+          <a href="https://github.com/fabianofrank/apple-income">
+            <figure className="my-6"><img src={apple} alt="Financial Statements Website" /></figure>
+            <div className="mx-3 flex flex-col">
+              <h2 className="text-[#e6e6e9] font-semibold">Financial Statements</h2>
+              <p className="text-[#e6e6e9] font-light">Financial Modeling Prep API was used to create a single page application that displays each Apple income statement of the last 5 years.</p>
+              <div className="self-center">
+                <div className="px-6 pt-4 pb-2">
+                  <span className="inline-block bg-[#e6e6e9] rounded-full px-3 py-1 text-sm font-semibold text-[#1a1b1b] mr-2 mb-2">#react</span>
+                  <span className="inline-block bg-[#e6e6e9] rounded-full px-3 py-1 text-sm font-semibold text-[#1a1b1b] mr-2 mb-2">#redux</span>
+                  <span className="inline-block bg-[#e6e6e9] rounded-full px-3 py-1 text-sm font-semibold text-[#1a1b1b] mr-2 mb-2">#javascript</span>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+
+        <div className="w-96 m-6 shadow-xl bg-gray-200 backdrop-filter backdrop-blur-lg bg-opacity-10 rounded-3xl">
+          <a href="https://github.com/fabianofrank/rails-wallet">
+            <figure className="my-6"><img src={wallet} alt="Investments Wallet Website" /></figure>
+            <div className="mx-3 flex flex-col">
+              <h2 className="text-[#e6e6e9] font-semibold">My Wallet</h2>
+              <p className="text-[#e6e6e9] font-light">My Wallet is meant to help you organize your investments across different categories.</p>
+              <div className="self-center">
+                <div className="px-6 pt-4 pb-2">
+                  <span className="inline-block bg-[#e6e6e9] rounded-full px-3 py-1 text-sm font-semibold text-[#1a1b1b] mr-2 mb-2">#react</span>
+                  <span className="inline-block bg-[#e6e6e9] rounded-full px-3 py-1 text-sm font-semibold text-[#1a1b1b] mr-2 mb-2">#redux</span>
+                  <span className="inline-block bg-[#e6e6e9] rounded-full px-3 py-1 text-sm font-semibold text-[#1a1b1b] mr-2 mb-2">#javascript</span>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+
+        <div className="w-96 m-6 shadow-xl bg-gray-200 backdrop-filter backdrop-blur-lg bg-opacity-10 rounded-3xl">
+          <a href="https://github.com/fabianofrank/pokemon-api">
+            <figure className="my-6"><img src={pokemon} alt="Pokedex Website" /></figure>
+            <div className="mx-3 flex flex-col">
+              <h2 className="text-[#e6e6e9] font-semibold">Pokedex</h2>
+              <p className="text-[#e6e6e9] font-light">It&apos;s a nostalgic interpretation of a pokedex, and we hope you enjoy it as much as we did putting it together.</p>
+              <div className="self-center">
+                <div className="px-6 pt-4 pb-2">
+                  <span className="inline-block bg-[#e6e6e9] rounded-full px-3 py-1 text-sm font-semibold text-[#1a1b1b] mr-2 mb-2">#javascript</span>
+                  <span className="inline-block bg-[#e6e6e9] rounded-full px-3 py-1 text-sm font-semibold text-[#1a1b1b] mr-2 mb-2">#api</span>
+                  <span className="inline-block bg-[#e6e6e9] rounded-full px-3 py-1 text-sm font-semibold text-[#1a1b1b] mr-2 mb-2">#css</span>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+
+        <div className="w-96 m-6 shadow-xl bg-gray-200 backdrop-filter backdrop-blur-lg bg-opacity-10 rounded-3xl">
+          <a href="https://github.com/fabianofrank/bookstore">
+            <figure className="my-6"><img src={bookstore} alt="Bookstore Website" /></figure>
+            <div className="mx-3 flex flex-col">
+              <h2 className="text-[#e6e6e9] font-semibold">Bookstore</h2>
+              <p className="text-[#e6e6e9] font-light">This is a project developed in React and Rudux, that creates a Bookstore to add, delete, edit and comment books based on a category list.</p>
+              <div className="self-center">
+                <div className="px-6 pt-4 pb-2">
+                  <span className="inline-block bg-[#e6e6e9] rounded-full px-3 py-1 text-sm font-semibold text-[#1a1b1b] mr-2 mb-2">#javascript</span>
+                  <span className="inline-block bg-[#e6e6e9] rounded-full px-3 py-1 text-sm font-semibold text-[#1a1b1b] mr-2 mb-2">#react</span>
+                  <span className="inline-block bg-[#e6e6e9] rounded-full px-3 py-1 text-sm font-semibold text-[#1a1b1b] mr-2 mb-2">#redux</span>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+
+        <div className="w-96 m-6 shadow-xl bg-gray-200 backdrop-filter backdrop-blur-lg bg-opacity-10 rounded-3xl">
+          <a href="https://github.com/fabianofrank/space-travelers">
+            <figure className="my-6"><img src={spacetravelers} alt="Space Travelers Website" /></figure>
+            <div className="mx-3 flex flex-col">
+              <h2 className="text-[#e6e6e9] font-semibold">Space Travelers</h2>
+              <p className="text-[#e6e6e9] font-light">The application will allow users to book rockets and join selected space missions.</p>
+              <div className="self-center">
+                <div className="px-6 pt-4 pb-2">
+                  <span className="inline-block bg-[#e6e6e9] rounded-full px-3 py-1 text-sm font-semibold text-[#1a1b1b] mr-2 mb-2">#api</span>
+                  <span className="inline-block bg-[#e6e6e9] rounded-full px-3 py-1 text-sm font-semibold text-[#1a1b1b] mr-2 mb-2">#react</span>
+                  <span className="inline-block bg-[#e6e6e9] rounded-full px-3 py-1 text-sm font-semibold text-[#1a1b1b] mr-2 mb-2">#redux</span>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+
+      </div>
+
     </div>
   </div>
 );
